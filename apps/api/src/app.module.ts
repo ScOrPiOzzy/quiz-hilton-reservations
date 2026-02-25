22077fdf9b5a302b377e668f31eb93b83de38a7d
Zenon <zenon.zhang@budweiserapac.com>
2026-02-25 16:20:08 +0800

    feat: 添加用户管理模块

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ResturantModule } from './resturant/resturant.module';
 import { HotelModule } from './hotel/hotel.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ResturantModule, HotelModule, ReservationModule],
+  imports: [ResturantModule, HotelModule, ReservationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
