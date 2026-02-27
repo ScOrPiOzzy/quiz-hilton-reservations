20b971fbc64240cb18b72dfe896359a86a191f49
Zenon <zenon.zhang@budweiserapac.com>
2026-02-27 21:37:18 +0800

    feat: 后端服务支持跨域访问

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import * as Couchbase from 'couchbase';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
   const app = await NestFactory.create(AppModule);
+  Couchbase.shutdownLogger();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   // 全局启用验证
   app.useGlobalPipes(
@@ -18,6 +27,7 @@ async function bootstrap() {
   // 全局响应拦截器
   app.useGlobalInterceptors(new ResponseInterceptor());
 
+  logger.log(`端口运行在 ${process.env.PORT ?? 3000}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
